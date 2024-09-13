f9a27163d8dc35333c1bf883495e083f1889c5f2
WillSantos08 <william.santana2@outlook.com>
2024-09-13 20:53:17 -0300

    Updated Commit

--- Desenvolvimento de Técnicas de Algorítmos/exec6.java
+++ Desenvolvimento de Técnicas de Algorítmos/exec6.java
@@ -3,7 +3,7 @@ public class exec6 {
     public static void main (String args []) {
         Scanner obj = new Scanner(System.in);
         
-        int A, B, X;
+        int A, B, C, X;
 
         System.out.println("Informe o valor 'A':");
         A = obj.nextInt();
@@ -14,7 +14,7 @@ public class exec6 {
         System.out.println("Informe o valor 'X':");
         X = obj.nextInt();
 
-        if (X => 10) {
+        if (X >= 10) {
             C = A + B;
         }
         else {
