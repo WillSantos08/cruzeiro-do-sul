e86da71e616f828a712a650d926ba0d75a33b9d1
WillSantos08 <william.santana2@outlook.com>
2024-09-20 21:01:38 -0300

    First commit

---
+++ Desenvolvimento de Técnicas de Algorítmos/exec8.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class main8 {
+    public static void main (String args[]) {
+        Scanner obj = new Scanner(System.in);
+
+        int tempo;
+        double salario, resultado,  bonus;
+        String sexo = "";
+
+        System.out.println("Informe seu sexo: ");
+        sexo = obj.nextLine();
+
+        System.out.println("Informe seu tempo de empresa em anos: ");
+        tempo = obj.nextInt;
+
+        if (sexo.equalsIgnoreCase("Homem") && tempo >= 15) {
+            bonus = salario * 0.25;
+            resultado = salario + bonus;
+            System.out.println("Seu salário após o bonus é de R$" + resultado);
+        } else  if (sexo.equalIgnoreCase("Mulher") && tempo => 10) {
+            bonus = salario * 0.30;
+            resultado = salario + bonus;
+            System.out.println("Seu salário após o bonus é de R$" + resultado);
+        } else if ((sexo.equalIgnoreCase("Homem") && tempo != 15) || (sexo.equalIgnoreCase("Mulher") && tempo != 10)) {
+            bonus = 500;
+            resultado = salario + bonus;
+            System.out.println("Seu salário após o bonus é de R$" + resultado);
+        } else {
+            System.out.println("Você não existe!");
+        }
+    }
+}
