531b192a584b000e6d282cdffe6c69286e417cf9
WillSantos08 <william.santana2@outlook.com>
2024-10-07 20:07:39 -0300

    Updated Commit

--- Programação Web/Aula 07-10/index.js
+++ Programação Web/Aula 07-10/index.js
@@ -3,8 +3,13 @@ function escolha() {
     document.write("<select multiple style='height: 200px; width: 100px;'>")
 
     for (var n1 = 0; n1 <= 10; n1++) {
-        document.write("<option>" + numero + "x" + n1 + "=" + n1 * numero + "</option>");
+        document.write("<option>" + numero + " x " + n1 + " = " + (n1 * numero) + "</option>");
     }
 
     document.write("</select>");
+    document.write("<br><input type='button' value='Voltar' onclick='voltar()'>");
+}
+
+function voltar() {
+    window.location.reload(true);
 }
