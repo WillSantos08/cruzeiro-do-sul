70ce69e599b05d6d31bfaea8c13ffa12baba225b
WillSantos08 <william.santana2@outlook.com>
2024-10-07 21:04:49 -0300

    Updated Commit

--- Programação Web/Aula 07-10/index.js
+++ Programação Web/Aula 07-10/index.js
@@ -2,7 +2,7 @@ function escolha() {
     var numero = prompt("Insira um número: ");
         if (numero == null || numero == 0) {
             alert("Digite um valor válido!");
-            var numero = prompt("Insira um número: ");
+            return(escolha);
         }
 
     document.write("<select multiple style='height: 200px; width: 100px;'>")
