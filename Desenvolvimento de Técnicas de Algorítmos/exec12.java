acdf654c5292d670b2ea3733d41445736bcb2b7d
WillSantos08 <william.santana2@outlook.com>
2024-10-25 20:35:49 -0300

    First Commit

--- Desenvolvimento de Técnicas de Algorítmos/exec12.java
+++ Desenvolvimento de Técnicas de Algorítmos/exec12.java
@@ -24,7 +24,7 @@ public class exec12 {
             }
         }
 
-        if (count > 3) {
+        if (escolhaJogador != numSorteado) {
             System.out.println("VOCÊ PERDEU O JOGO!");
         }
     }
