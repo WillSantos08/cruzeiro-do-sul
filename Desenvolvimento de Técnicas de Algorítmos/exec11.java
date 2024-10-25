45d1abea63c3c70ae2537ca52a1317e87a8ea29b
WillSantos08 <william.santana2@outlook.com>
2024-10-25 19:50:34 -0300

    First Commit

---
+++ Desenvolvimento de Técnicas de Algorítmos/exec11.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class exec11() {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+
+        int nota = 0, idade = 0, qtdRegular = 0, qtdBom = 0, qtdOtimo = 0;
+        double contaIdade = 0, somaIdade = 0;
+        String SN = "s";
+
+        // Loop de Repetição
+        while (SN.equals("s")) {
+            System.out.println("Informe a sua idade: ");
+            idade = obj.nextInt();
+
+            System.out.println("Informe sua nota: ");
+            nota = obj.nextInt();
+
+            if (nota == 3) {
+                qtdOtimo++;
+                contaIdade++;
+                somaIdade = somaIdade + idade;
+            } else if (nota == 2) {
+                qtdBom++;
+            } else if (nota == 1) {
+                qtdRegular++;
+            } else {
+                System.out.println("O valor inserído não é válido!");
+            }
+
+            System.out.println("Deseja continuar a computar dados? (S/N)");
+            SN = obj.next();
+        }
+
+        System.out.println("Média das Idades - " + somaIdade/contaIdade + "\nQuantidade Regular - " + qtdRegular + "\nQuantidade Ótimo - " + qtdOtimo);
+    }
+}
